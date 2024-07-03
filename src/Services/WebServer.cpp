9df0f54953a4e77336fe3fa4d6f7f0c0f0c9cd49
Aaron Averill <aaron@expensepath.com>
2024-07-03 13:33:54 -0400

    Clean up home page

--- src/Services/WebServer.cpp
+++ src/Services/WebServer.cpp
@@ -13,35 +13,33 @@
 #include "ESPAsyncWebServer.h"
 
 namespace Services {
-  class RequestHandler : public AsyncWebHandler {
+  String getPage() {
+    String page = "<!DOCTYPE HTML>";
+    page += "<html>";
+    page += "<head>";
+    page += "<style>";
+    page += "html{background-color:#000;color:#FFF;font-family:sans-serif;}";
+    page += ".text-center{text-align:center}";
+    page += "</style>";
+    page += "</head>";
+    page += "<body>";
+    page += "<h1 class='text-center'>ESP Spatial LED</h1>";
+    page += "</body>";
+    page += "</html>";
+    return page;
+  }
+
+  class CaptiveRequestHandler : public AsyncWebHandler {
     public:
-      RequestHandler() {}
-      virtual ~RequestHandler() {}
+      CaptiveRequestHandler() {}
+      virtual ~CaptiveRequestHandler() {}
 
       bool canHandle(AsyncWebServerRequest *request){
         return true;
       }
 
       void handleRequest(AsyncWebServerRequest *request) {
-        AsyncResponseStream *response = request->beginResponseStream("text/html");
-        response->print(getPage());
-        request->send(response);
-      }
-
-    private:
-      String getPage() {
-        String page = "<!DOCTYPE HTML>";
-        page += "<html>";
-        page += "<head>";
-        page += "<style>";
-        page += "html{background-color:#000;color:#FFF;}";
-        page += "</style>";
-        page += "</head>";
-        page += "<body>";
-        page += "<h1>ESP Spatial LED</h1>";
-        page += "</body>";
-        page += "</html>";
-        return page;
+        request->redirect("http://" + WiFi.softAPIP().toString());
       }
   };
 
@@ -51,11 +49,35 @@ namespace Services {
     WiFi.softAP(ssid);
     dnsServer->start(53, "*", WiFi.softAPIP());
 
-    server->addHandler(new RequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
+    server->on("/", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      AsyncResponseStream *response = request->beginResponseStream("text/html");
+      response->print(getPage());
+      request->send(response);
+    });
+    server->addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
     server->begin();
   }
 
   void WebServer::loop() {
     dnsServer->processNextRequest();
+  }
+
+  void renderHome(AsyncWebServerRequest *request) {
+    String html = R"(
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+  html{background-color:#000;color:#FFF;font-family:sans-serif;}
+  .text-center{text-align:center}
+</style>
+</head>";
+<body>
+<h1 class='text-center'>ESP Spatial LED</h1>
+</body>
+</html>)";
+    AsyncResponseStream *response = request->beginResponseStream("text/html");
+    response->print(html);
+    request->send(response);
   }
 }
