392f430406bea6952b6d159a577bd71a1c24d5c6
Aaron Averill <aaron@expensepath.com>
2024-07-04 13:12:53 -0400

    Simplify

--- src/Services/WebRequestHandler.h
+++ src/Services/WebRequestHandler.h
@@ -3,11 +3,10 @@
 #include "ESPAsyncWebServer.h"
 
 namespace Services {
-  class WebRequestHandler {
-    public:
-      WebRequestHandler(const char* route, WebRequestMethodComposite method, ArRequestHandlerFunction handler): route(route), method(method), handler(handler) {}
-      const char* route;
-      WebRequestMethodComposite method;
-      ArRequestHandlerFunction handler;
+  struct WebRequestHandler {
+    WebRequestHandler(const char* route, WebRequestMethodComposite method, ArRequestHandlerFunction handler): route(route), method(method), handler(handler) {}
+    const char* route;
+    WebRequestMethodComposite method;
+    ArRequestHandlerFunction handler;
   };
 }
