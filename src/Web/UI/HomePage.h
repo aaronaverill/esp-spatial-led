0c7191a49c21cd6a77320b038459668e123fc77a
Aaron Averill <aaron@expensepath.com>
2024-07-11 12:45:39 -0400

    Serve content pages with gzip encoding

--- src/Web/UI/HomePage.h
+++ src/Web/UI/HomePage.h
@@ -7,6 +7,12 @@
 #include "Content/app_js.h"
 
 namespace Web { namespace UI {
+  void sendGZipResponse(AsyncWebServerRequest *request, const char *contentType, const uint8_t *content, size_t length) {
+    AsyncWebServerResponse *response = request->beginResponse_P(200, contentType, content, length);
+    response->addHeader("Content-Encoding", "gzip");
+    request->send(response);
+  }
+
   /**
    * HTTP Request handler class for website requests.
    * Returns an HTML page that is a single page application (SPA) which controls the software.
@@ -24,25 +30,19 @@ namespace Web { namespace UI {
           return;
         }
 
-        AsyncWebServerResponse *response;
-        const char *contentType = "text/html";
-        const char *content = nullptr;
         if (request->url().equals("/")) {
-          content = index_html;
+          sendGZipResponse(request, "text/html", index_html, index_html_length);
+          return;
         } else if (request->url().equals("/app.css")) {
-          contentType = "text/css";
-          content = app_css;
+          sendGZipResponse(request, "text/css", app_css, app_css_length);
+          return;
         } else if (request->url().equals("/app.js")) {
-          contentType = "text/javascript";
-          content = app_js;
+          sendGZipResponse(request, "text/javascript", app_js, app_js_length);
+          return;
         }
-        if (content != nullptr) {
-          response = request->beginResponse_P(200, contentType, content);
-          request->send(response);
-        } else {
-          Serial.println("Page Not Found: " + request->url());
-          request->send(404);
-        }
+        
+        Serial.println("Page Not Found: " + request->url());
+        request->send(404);
       } 
   };
 }}
