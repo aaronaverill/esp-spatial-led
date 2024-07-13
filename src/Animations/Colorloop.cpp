f9114e10c79dc3e749168042137326d0446650a6
Aaron Averill <aaron@expensepath.com>
2024-07-13 10:15:55 -0400

    Implement colorloop with EVERY_N_MILLIS_I() to prevent the color jumping when BPM is changed.

--- src/Animations/Colorloop.cpp
+++ src/Animations/Colorloop.cpp
@@ -3,12 +3,45 @@
 #include <FastLED.h>
 
 namespace Animations {
+  Colorloop::Colorloop(Services::ILedDriverAnimationContext& context): Animation(context, "Colorloop") {
+    setBpm(bpm);
+  }
+
+  void Colorloop::getFields(Web::UI::FieldsInfo& fields) {
+    fields.addSlider("speed", "Speed", 1, 60, "${val} bpm");
+  }
+
+  void Colorloop::getSettings(JsonObject& settings) {
+    settings["speed"] = bpm;
+  }
+
+  void Colorloop::setSettings(const JsonObject& settings) {
+    Serial.println("Colorloop::setSettings");
+    if (settings["speed"]) {
+      setBpm(settings["speed"]);
+    }
+  }
+
   void Colorloop::renderFrame() {
-    hue = beat8(bpm);
+    EVERY_N_MILLIS_I(beat,everyMillis) {
+      hue += hueIncrement;
+    }
     Animation::renderFrame();
   }
 
   void Colorloop::renderLed(int index) {
     context.hsv(CHSV(hue,255,255));
+  }
+
+  void Colorloop::setBpm(byte bpm) {
+    this->bpm = bpm;
+    hueIncrement = 1;
+    float timeBetween = 234.375/bpm;
+    // Don't update more than once every 20 milliseconds
+    if (timeBetween < 20) {
+      hueIncrement = 20/timeBetween;
+      timeBetween *= hueIncrement;
+    }
+    everyMillis = timeBetween;
   }
 }
--- src/Animations/Colorloop.h
+++ src/Animations/Colorloop.h
@@ -8,11 +8,17 @@
 namespace Animations {
   class Colorloop: public Animation {
     public:
-      Colorloop(Services::ILedDriverAnimationContext& context): Animation(context, "Colorloop") {}
+      Colorloop(Services::ILedDriverAnimationContext& context);
+      void getFields(Web::UI::FieldsInfo& fields);
+      void getSettings(JsonObject& settings);
+      void setSettings(const JsonObject& settings);
       void renderFrame();
       void renderLed(int index);
     private:
-      uint16_t bpm = 10;
+      void setBpm(byte bpm);
+      byte bpm = 10;
+      uint32_t everyMillis;
+      byte hueIncrement;
       byte hue;
   };
 }
