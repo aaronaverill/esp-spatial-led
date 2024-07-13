d174e7a24ee92d2878133441a0bdb60f96c467cc
Aaron Averill <aaron@expensepath.com>
2024-07-13 08:19:27 -0400

    Include npm install in pre-build step to install node packages Commenting bundling process

--- build/webbundle.py
+++ build/webbundle.py
@@ -1,5 +1,11 @@
 Import("env")
 
+# Install the necessary node packages for the pre-build asset bundling script
+env.Execute("npm install")
+
+# Call the bundling script
 exitCode = env.Execute("node build/webbundle.mjs")
+
+# If it failed, abort the build
 if (exitCode):
   Exit(exitCode)
