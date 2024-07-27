48c041970cb45b533dfa29bc68d2874d988a56ec
Aaron Averill <aaron@expensepath.com>
2024-07-26 20:15:49 -0400

    Simplify RainbowPlane animation.

--- src/Animations/Spatial/RainbowPlane.cpp
+++ src/Animations/Spatial/RainbowPlane.cpp
@@ -1,5 +1,4 @@
 #include "RainbowPlane.h"
-#include "Algorithm/Matrix3D.h"
 
 #include <FastLED.h>
 
@@ -7,28 +6,17 @@ namespace Animations { namespace Spatial {
   RainbowPlane::RainbowPlane(Services::ILedDriverAnimationContext& context):
     SpatialAnimation(context, "Rainbow Plane") {
     setBpm(bpm);
-    updateRotation();
   }
 
   void RainbowPlane::getFields(Web::UI::FieldsInfo& fields) {
     fields.addSlider("speed", "Speed", 1, 60, "`${val} bpm`", 0);
     fields.addSlider("repeat", "Repeat", 1, 5*6,
       "switch(parseInt(val)){case 1:case 5:val+'/6';break;case 2:case 4:(val/2)+'/3';break;case 3:'1/2';break;default:Math.round(val/6);}");
-    const char axis[3] = { 'X', 'Y', 'Z'};
-    for(uint8_t i = 1; i < 3; i++) {
-      String id = String("rotation") + String((int)i);
-      String label = String(axis[i]) + String(" Rotation");
-      fields.addSlider(id, label, 0, 360, "val+'&deg;'", 0);
-    }
   }
 
   void RainbowPlane::getSettings(JsonObject& settings) const {
     settings["speed"] = bpm;
     settings["repeat"] = repeat6;
-    for(uint8_t i = 1; i < 3; i++) {
-      String id = String("rotation") + String((int)i);
-      settings[id] = rotation[i]/CoordinateMax;
-    }
   }
 
   void RainbowPlane::setSettings(const JsonObject& settings) {
@@ -41,18 +29,6 @@ namespace Animations { namespace Spatial {
         repeat6 = round((float)repeat6/6)*6;
       }
     }
-    bool setRotation = false;
-    for(uint8_t i = 1; i < 3; i++) {
-      String id = String("rotation") + String((int)i);
-      if (!settings[id].isNull()) {
-        rotation[i] = ((float)settings[id])*CoordinateMax;
-        setRotation = true;
-      }
-    }
-    
-    if (setRotation) {
-      updateRotation();
-    }
   }
 
   void RainbowPlane::renderFrame() {
@@ -64,11 +40,7 @@ namespace Animations { namespace Spatial {
 
 
   void RainbowPlane::renderLed(int index, const Coordinate& coordinate) {
-    CoordinateType point[4] = {coordinate.x, coordinate.y, coordinate.z, 1};
-    CoordinateType transformedPoint[4];
-    rotationMatrix.multiplyVec(point, transformedPoint);
-
-    byte offset = transformedPoint[0]*256*repeat6/CoordinateMax;
+    byte offset = 255.f * ((float) coordinate.z / CoordinateMax) * ((float)repeat6 / 6.f);
     context.hsv(hue+offset, 255, 255);
   }
 
@@ -82,9 +54,5 @@ namespace Animations { namespace Spatial {
       timeBetween *= hueIncrement;
     }
     everyMillis = timeBetween;
-  }
-
-  void RainbowPlane::updateRotation() {
-    rotationMatrix = Algorithm::Matrix3D::rotation(rotation[0], rotation[1], rotation[2]);
   }
 }}
--- src/Animations/Spatial/RainbowPlane.h
+++ src/Animations/Spatial/RainbowPlane.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "Animations/Spatial/SpatialAnimation.h"
 #include "Services/ILedDriverAnimationContext.h"
-#include "Algorithm/Matrix3D.h"
 
 namespace Animations { namespace Spatial {
   class RainbowPlane: public SpatialAnimation {
@@ -14,13 +13,10 @@ namespace Animations { namespace Spatial {
       void renderLed(int index, const Coordinate& coordinate);
     private:
       void setBpm(byte bpm);
-      void updateRotation();
       byte bpm = 10;
       uint32_t everyMillis;
       byte hueIncrement;
       byte hue;
       byte repeat6 = 6; // Number of times the rainbow repeats in 1/6ths
-      CoordinateType rotation[3] = {0, 0, 0};
-      Algorithm::Matrix3D rotationMatrix;
   };
 }}
