3cb75f03f8d76f453d339d5e7a47411240f2e010
Aaron Averill <aaron@expensepath.com>
2024-07-27 21:40:48 -0400

    Palette methods

--- src/Palette.h
+++ src/Palette.h
@@ -9,12 +9,34 @@
  */
 class Palette {
   public:
-    Palette(const char *name): name(name) {}
+    struct GradientStop {
+      GradientStop(byte percent, byte r, byte g, byte b): percent(percent), rgb(r, g, b) {}
 
-    class GradientStop {
       byte percent;
       CRGB rgb;
     };
+
+    Palette(const char *name): name(name) {}
+
+    const std::vector<GradientStop>& getStops() const { return stops; }
+
+    void addStop(byte percent, byte r, byte g, byte b) {
+      size_t insertAt = 0;
+      while (insertAt < stops.size() && percent > stops[insertAt].percent) {
+        insertAt++;
+      }
+      GradientStop stop(percent, r, g, b);
+      if (insertAt < stops.size()) {
+        if (stops[insertAt].percent == percent) {
+          stops[insertAt].rgb = stop.rgb;
+        } else {
+          stops.insert(stops.begin() + insertAt, stop);
+        }
+      } else {
+        stops.push_back(stop);
+      }
+    }
+
   private:
     const char *name;
     std::vector<GradientStop> stops;
